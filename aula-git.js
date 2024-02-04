137f9fa3df31699908ab6658c346690b82e8a18e
isabeladomingos <domingosisabela@icloud.com>
2024-02-04 19:42:27 -0300

    alterações e correção

--- aula-git.js
+++ aula-git.js
@@ -1,4 +1,10 @@
-console.log(Aula Git);
+console.log ("Aula Git");
 //git init - inicia um novo repositório.
+
 //git clone - "clona" repositório do servidor para local.
-//git clone: (no github) Code > Clone > SSH > copiar URL > (no vs code) git clone + colar url copiada > git status > para saber se deu tudo certo.
+//git clone: (no github) Code > Clone > SSH > copiar URL > (no vs code) git clone + colar url copiada >
+// git status > para saber se deu tudo certo.
+
+//git add: enviar arquivo para a área de preparação = Stage.
+//enviar arquivo específico - git add + nome exato do arquivo
+//enviar todos os arquivos - git add .
