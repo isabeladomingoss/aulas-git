771f5c244e36e97543800dc45690401b1197e879
isabeladomingos <domingosisabela@icloud.com>
2024-02-04 19:39:39 -0300

    Aula Git

---
+++ aula-git.js
@@ -0,0 +1,4 @@
+console.log(Aula Git);
+//git init - inicia um novo repositório.
+//git clone - "clona" repositório do servidor para local.
+//git clone: (no github) Code > Clone > SSH > copiar URL > (no vs code) git clone + colar url copiada > git status > para saber se deu tudo certo.
