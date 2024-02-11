a87f1be3cd0700a447f9f1c7982101af73e5e4a0
isabeladomingos <domingosisabela@icloud.com>
2024-02-11 16:35:35 -0300

    novidades

---
+++ variaveis-tipos-corretos.js
@@ -0,0 +1,6 @@
+let x = "10" , y = 2;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`); //A soma de 10 e 2 é 12
+
+//como colocou o 10 em string ao invés de number o java script vai concatenar
---
+++ variaveis-x-++.js
@@ -0,0 +1,14 @@
+{
+    let x = 10;
+let y = x++;
+console.log("X:", x);//11
+console.log("Y:", y);//10
+}
+let x = 10;
+//x = x + 1;
+//x += 1;
+let y = ++x;
+console.log("X:", x);//11
+console.log("Y:", y);//11
+
+//quando o ++ vem primeiro soma primeiro.
